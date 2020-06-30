0ba901bcfe5d82bdd3d3de3407e91d5df875b4b4
slewie <slewie@users.noreply.github.com>
2020-06-30 12:31:48 +0500

    Add app

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from config import Configuration
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config.from_object(Configuration)
+
+mongo = MongoEngine(app)
+mongo.db.user.insert({'name': 'name'})
+if __name__ == '__main__':
+    app.run()
